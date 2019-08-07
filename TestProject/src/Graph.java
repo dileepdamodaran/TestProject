dda95e338bd81dbf3cff22736af7b92d3825d671
DD <dileep.damodaran.n@gmail.com>
2019-08-07 06:01:00 -0400

    Test commit

---
+++ TestProject/src/Graph.java
@@ -0,0 +1,52 @@
+import java.util.LinkedList;
+
+public class Graph {
+	//Vertex
+	int V;
+	
+	LinkedList<Integer> adjListArray[];
+	
+	// constructor  
+    Graph(int V) 
+    { 
+        this.V = V; 
+          
+        // define the size of array as  
+        // number of vertices 
+        adjListArray = new LinkedList[V]; 
+          
+        // Create a new list for each vertex 
+        // such that adjacent nodes can be stored 
+        for(int i = 0; i < V ; i++){ 
+            adjListArray[i] = new LinkedList<>(); 
+        } 
+    } 
+    
+ // Adds an edge to an undirected graph 
+    static void addEdge(Graph graph, int src, int dest) 
+    { 
+        // Add an edge from src to dest.  
+        graph.adjListArray[src].add(dest); 
+          
+        // Since graph is undirected, add an edge from dest 
+        // to src also 
+        graph.adjListArray[dest].add(src); 
+    } 
+
+ // A utility function to print the adjacency list  
+    // representation of graph 
+    static void printGraph(Graph graph) 
+    {        
+        for(int v = 0; v < graph.V; v++) 
+        { 
+            System.out.println("Adjacency list of vertex "+ v); 
+            System.out.print("head"); 
+            for(Integer pCrawl: graph.adjListArray[v]){ 
+                System.out.print(" -> "+pCrawl); 
+            } 
+            System.out.println("\n"); 
+        } 
+    } 
+       
+}
+
